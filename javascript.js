11de9ff9e92a8ceff26312969714b5de893bd8d0
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-12-14 18:15:15 +0300

    working number and clear buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,31 @@
-const screen = document.querySelector(".screen")
+let lowScreen = document.querySelector(".lower");
+let upScreen = document.querySelector(".upper");
+let calculator = document.querySelector(".Calculator");
+let buttons = document.querySelectorAll("button");
 
-function operate(x,y) {
-    
+function operate(x, y, operator) {
+  if (operator == "+") {
+    return x + y;
+  }
+  if (operator == "-") {
+    return x - y;
+  }
+  if (operator == "*") {
+    return x * y;
+  } else {
+    return x / y;
+  }
 }
+
+buttons.forEach((x) => {
+  if (!isNaN(Number(x.textContent)) || x.textContent === ".") {
+    x.addEventListener("click", () => {
+      lowScreen.textContent += x.textContent;
+    });
+  }
+  else if (x.textContent === "Clear"){
+    x.addEventListener("click", () => {
+      lowScreen.textContent= ""
+    })
+  }
+});
